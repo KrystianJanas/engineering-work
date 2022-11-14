e476ea4f26145d06771e483a0da2d5ba364e9168
Krystian Janas <postofficenex@gmail.com>
2022-11-14 22:24:10 +0100

    add button - component

---
+++ frontend/core/components/compounds/AddButton/components/add-button.tsx
@@ -0,0 +1,32 @@
+import styled from '@emotion/styled';
+
+import { Text } from '~/components/atoms/typography';
+import { AddButtonTypes } from '~/components/compounds/AddButton/add-button.types';
+import { Layout } from '~/components/molecules/layout';
+import { getRem } from '~/styles/utils';
+
+const StyledLayout = styled(Layout)`
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export const AddButton = ({ onClick }: AddButtonTypes) => {
+  return (
+    <StyledLayout
+      background="var(--button-background)"
+      width={40}
+      height={40}
+      boxShadow="0px 2px 4px rgba(0, 0, 0, 0.16)"
+      borderRadius="10px"
+      onClick={onClick && onClick}
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Text color="var(--white)" weight={300} size={getRem(32)}>
+        +
+      </Text>
+    </StyledLayout>
+  );
+};
--- frontend/core/styles/colors.tsx
+++ frontend/core/styles/colors.tsx
@@ -2,6 +2,7 @@ import { css } from '@emotion/react';
 
 export const colorsStyles = css`
   :root {
+    --white: #ffffff;
     --button-background: #12b9ac;
   }
 `;
